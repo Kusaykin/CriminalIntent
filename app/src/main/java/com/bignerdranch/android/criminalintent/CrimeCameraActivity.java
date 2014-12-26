af296d595e86a2bdfa1ff48a085be7d7ef0b27cd
Kusaykin <susanindin@gmail.com>
2014-12-26 14:16:57 +0600

    1

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeCameraActivity.java
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+/**
+ * Created  on 24.12.2014.
+ */
+public class CrimeCameraActivity extends SingleFragmentActivity  {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+// Скрытие заголовка окна.
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+// Скрытие панели состояния и прочего оформления уровня ОС
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected Fragment createFragment() {
+		return new CrimeCameraFragment();
+	}
+}
